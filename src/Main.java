64dcc6fce2a67cc29ff062866eff76569a105bdd
Shabrov A.L. QA-72 <valmar.chab@gmail.com>
2023-09-26 16:32:47 +0500

    Zadanie

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
 
 public class Main {
     public static void main(String[] args) {
+        int base = 1000;
+        int deposit;
+        int score;
 
+        score = 100;
+        deposit = 300;
+        if (deposit + score > base) {
+            score = deposit / 100 + score + deposit;
+        } else {
+            score = score + deposit;
+        }
+        System.out.println("На счёте: " + score);
+
+        score = 100;
+        deposit = 1100;
+        if (deposit + score > base) {
+            score = deposit / 100 + score + deposit;
+        } else {
+            score = score + deposit;
+        }
+        System.out.println("На счёте: " + score);
     }
 }
